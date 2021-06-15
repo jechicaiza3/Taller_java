37953c2202b8efd7ff47da0a8c6f0380cea16813
jefferson chicaiza <jchicaiza151@gmail.com>
2021-06-15 00:53:08 -0500

    Taller#1_java3725

---
+++ src/Dominioo/Curso.java
@@ -0,0 +1,60 @@
+
+package Dominioo;
+
+
+public class Curso {
+    
+
+        private double [] notas;
+        private double promedio;
+        private int estudiantesEncimaPromedio;
+    private int estudianEncimaPromedio;
+        public Curso(){
+            notas = new double[12];
+        
+       
+        }
+
+    public  void setNotas(double notas[]){
+        this.notas = notas;
+    }
+    public void mostrarNotas(){
+    for (int i=0;i<12;i++){
+System.out.println(" Nro del estudiante: "+this.notas[i]);  
+   }
+    }
+    public void setPromedio (){
+        double promedio=0;
+        for (int i=0;i<12;i++){
+      this.notas[0]=this.notas[0]+this.notas[i];
+    }
+     this.promedio=this.notas[0]/12;
+      }
+            
+  public void  setEstudianEncimaPromedio() {
+      int estudiantes = 0;
+      for (int i=0;i<12;i++){
+          if(this.notas[i]<this.getPromedio()){
+              estudiantes ++;
+          }
+      }
+      this.estudianEncimaPromedio=estudiantes; 
+     
+  }
+    public double getPromedio() {
+        return this.promedio;
+    }
+     
+    public int estudianEncimaPromedio() {
+    return this.estudianEncimaPromedio;
+    }
+
+ 
+    public void setPromedio(double[] promedio) {
+    }
+
+    public String getnroEstudiantesEncimaPromedio() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Test/Test_curso.java
@@ -0,0 +1,30 @@
+
+
+
+package Test;
+
+
+import Dominioo.Curso;
+
+public class Test_curso{
+    
+    public static void main (String[] args){
+       Curso cursoA = new Curso();    
+        double [] notas = new double [12];
+             for (int i=0;i<12;i++){
+             notas[i] = Math.random()*6;
+             }
+        //LlenaNotas(notas);     
+    cursoA.setNotas(notas);
+     cursoA.mostrarNotas();
+     cursoA.setPromedio();
+      System.out.println("Promedio del curso "+cursoA.getPromedio());
+      cursoA.setEstudianEncimaPromedio();
+      System.out.println("Encima del promedio: "+cursoA.estudianEncimaPromedio());
+   } 
+ public static void llenaNotas(double[] notas){
+ for (int i=0;i<12;i++){
+         notas[i] = Math.random()*6;
+}
+ }           
+   }
